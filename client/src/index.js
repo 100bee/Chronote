66a7c39fc9e188fdded48901c2bee8e7e0e9e541
100bee <wjdwltjr3939@naver.com>
2025-07-01 13:29:23 +0900

    sass변환 완료

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.sass'; // 💡 이 줄 추가 (index.sass가 src 폴더 안에 있어야 함)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
