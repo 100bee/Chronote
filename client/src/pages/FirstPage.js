b3b29e75a77d32928e7f0d9a3cee1c1f653ecfeb
sangwe0719 <ma0719@naver.com>
2025-07-08 14:00:35 +0900

    login-signup merge, firstpage logo

--- client/src/pages/FirstPage.js
+++ client/src/pages/FirstPage.js
@@ -1,15 +1,16 @@
 import { useNavigate } from 'react-router-dom';
+import '../css/firstpage.scss';
 
 function FirstPage() {
   const navigate = useNavigate();
 
   return (
     <div className="container">
-      <h1 className="title">ChroNote</h1>
+      <img src="/favicon3.png" alt="Chronote logo" className='logo'/>
       <p className="subtitle">작은 계획이 큰 변화를 만듭니다. 오늘 할 일을 적어보세요.</p>
-      <div className="button-group">
-        <button onClick={() => navigate('/signup')}>회원가입</button>
+      <div className="button-stack">
         <button onClick={() => navigate('/login')}>로그인</button>
+        <button onClick={() => navigate('/signup')}>회원가입</button>
       </div>
     </div>
   );
