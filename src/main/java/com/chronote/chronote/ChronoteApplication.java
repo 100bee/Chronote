78a7f58c83708cd4451e73b912344a358b5740c3
정지석 <wjdwltjr3939@naver.com>
2026-03-22 17:02:51 +0900

    init SpringBoot

---
+++ src/main/java/com/chronote/chronote/ChronoteApplication.java
@@ -0,0 +1,13 @@
+package com.chronote.chronote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronote/chronote/ChronoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronote.chronote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
